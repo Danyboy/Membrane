88ca0fce24958f6f611119a0df910dfcefb7d14a
Danil Mikhaylov <danil@etersoft.ru>
2013-10-14 01:17:11 +0400

    Added menu state selector

--- src/membrane/MemJFrame.java
+++ src/membrane/MemJFrame.java
@@ -10,8 +10,7 @@ public class MemJFrame extends JFrame {
     static int X = 40;
     static int Y = X ;
     public static JTextField quantityField;
-    private JButton start;
-    private JPanel j;
+    private JPanel myBottonJPanel;
     private Membrane m;
     static MemCanvas canvas;
     static MemJFrame mf;
@@ -19,25 +18,44 @@ public class MemJFrame extends JFrame {
     static int initialPressure     = 5;
     static int length              = 4;
     public static int quantity     = 0;
-    private JButton speed;
-    private JButton flow;
-    private JButton pressure;
-
+    private JButton speed, flow, pressure, start;
+    
     private void myJFrame() {
         canvas = new MemCanvas(X, Y, m);
         canvas.setPreferredSize(new Dimension(640, 640));
         canvas.setSize(X, Y);
  
-        j = new JPanel(new FlowLayout());
-        start = new JButton("Start");
+        myBottonJPanel = new JPanel(new FlowLayout());
+        start = new JButton("Generate random membrane with ");
         speed = new JButton("Speed");
         flow = new JButton("Flow");
         pressure = new JButton("Pressure");
-        quantityField = new JTextField("" + quantity);
-
-        j.add(start);
+        quantityField = new JTextField("" + quantity, 3);
+        JLabel tubesText = new JLabel("tubes");
+        
+        JPopupMenu stateSelectorJMenu = new JPopupMenu("Select state");
+        
+//        TODO set action
+        JMenuItem nothingItem = new JMenuItem("No calculation");
+        JMenuItem flowCofficientItem = new JMenuItem("Flou cofficient");
+        JMenuItem pressureItem = new JMenuItem("Pressure");
+        JMenuItem speedItem = new JMenuItem("Speed");
+        JMenuItem flowItem = new JMenuItem("Flow");
+        JMenuItem densityItem = new JMenuItem("Density");
+        
+        stateSelectorJMenu.add(nothingItem);
+        stateSelectorJMenu.add(flowCofficientItem);
+        stateSelectorJMenu.add(pressureItem);
+        stateSelectorJMenu.add(flowItem);
+        stateSelectorJMenu.add(densityItem);
+        
+        myBottonJPanel.add(start);
 //        j.add(pressure);
-        j.add(quantityField);
+        myBottonJPanel.add(quantityField);
+        myBottonJPanel.add(tubesText);
+        //TODO add menu to bar or jp
+        myBottonJPanel.setComponentPopupMenu(stateSelectorJMenu);
+        
 
 
         start.addActionListener(new ActionListener() {
@@ -50,9 +68,10 @@ public class MemJFrame extends JFrame {
         });
         setLocation(140, 60);
         add(canvas, BorderLayout.CENTER);
-        add(j, BorderLayout.PAGE_END);
+        add(myBottonJPanel, BorderLayout.PAGE_END);
+//        add()
         pack();
-        setDefaultCloseOperation(3);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
 
